32379e849fdefab2fd2440e5c6baaad11e88733e
Nico Hrubec <nicolas.hrubec@outlook.com>
2020-03-28 19:49:29 +0100

    Implemented: Range of input settings can now be defined for simulation.
    The simulator simulates every possible setting and stores the raw run results
    together with game stats and the given game settings in a pandas dataframe.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from src.simulation import Simulator
 
-s = Simulator(n_games=100, n_rounds=100, num_decks=6, deck_penetration=.5, player_type='basic', player_capital=1000)
-results = s.simulate()
+s = Simulator(n_games=100, n_rounds=100, num_decks=[4, 6], deck_penetration=[.4, .5], player_type=['basic'],
+              player_capital=[1000, 2000])
+s.simulate()
--- src/simulation.py
+++ src/simulation.py
@@ -4,7 +4,7 @@ from src.game import Game
 from src.players import player_factory
 
 
-class Simulator():
+class Simulator:
     def __init__(self, n_games, n_rounds, num_decks, deck_penetration, player_type, player_capital):
         self.n_games = n_games
         self.n_rounds = n_rounds
@@ -12,21 +12,43 @@ class Simulator():
         self.deck_penetration = deck_penetration
         self.player_type = player_type
         self.capital = player_capital
-        self.results = pd.DataFrame()
+        self.results = None
 
     def simulate(self):
-        for i in range(self.n_games):
-            player = player_factory(self.player_type, self.capital)
-            game = Game(self.num_decks, player, self.n_rounds, self.deck_penetration)
+        # simulate every defined setting
+        for player in self.player_type:
+            for n_decks in self.num_decks:
+                for penetration in self.deck_penetration:
+                    for player_capital in self.capital:
+                        setting_results = self.simulate_setting(player, n_decks, penetration, player_capital)
+
+                        if self.results is None:  # no results stored so far --> init results df
+                            self.results = pd.DataFrame(data=setting_results, columns=setting_results.columns)
+                        else:  # append to previous results
+                            self.results = pd.concat([self.results, setting_results])
+
+    def simulate_setting(self, player_type, num_decks, deck_penetration, capital):
+        setting_results = pd.DataFrame()
+
+        for i in range(self.n_games):  # simulate one game with given setting
+            player = player_factory(player_type, capital)
+            game = Game(num_decks, player, self.n_rounds, deck_penetration)
             game_results = game.play_game()
 
             run_id = 'run_' + str(i)
-            self.results[run_id] = game_results
+            setting_results[run_id] = game_results
 
-        self.calc_stats()
+        self.calc_stats(setting_results)
+        setting_results['player'] = player_type
+        setting_results['num_decks'] = num_decks
+        setting_results['deck_penetration'] = deck_penetration
+        setting_results['capital'] = capital
 
-        return self.results
+        return setting_results
 
-    def calc_stats(self):
-        self.results['step_mean'] = self.results.mean(axis=1)
-        self.results['step_std'] = self.results.std(axis=1)
+    @staticmethod
+    def calc_stats(df):
+        df['step_mean'] = df.mean(axis=1)
+        df['step_std'] = df.std(axis=1)
+
+        return df
