33c73a849ab2a16c5d0809837563e724cc2811ab
Nico Hrubec <nicolas.hrubec@outlook.com>
2020-03-29 11:55:53 +0200

    Add: Main method to main file.

--- src/main.py
+++ src/main.py
@@ -4,7 +4,8 @@ sys.path.append('..')
 
 from src.game_simulation.simulation import Simulator
 
-s = Simulator(n_games=100, n_rounds=100, num_decks=[4, 6], deck_penetration=[.4, .5], player_type=['basic'],
-              player_capital=[1000, 2000])
-s.simulate()
-s.save_results()
+if __name__ == '__main__':
+    s = Simulator(n_games=100, n_rounds=100, num_decks=[4, 6], deck_penetration=[.4, .5], player_type=['basic'],
+                  player_capital=[1000, 2000])
+    s.simulate()
+    s.save_results()
