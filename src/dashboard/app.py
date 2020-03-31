7278ae124af5f3c8f4ddac391802664c54af7dad
Nico Hrubec <nicolas.hrubec@outlook.com>
2020-03-31 21:07:38 +0200

    Added: Section with plots comparing summary stats of different simulation runs.

--- src/dashboard/app.py
+++ src/dashboard/app.py
@@ -32,18 +32,59 @@ def get_dropdown(title, id, options):
     ], style={'margin': '2%'})
 
 
+def get_summary_graph(df, capital, stat, title):
+    return html.Div([
+        html.Div([
+            html.H4(title)
+        ], style={"align": "bottom"}),
+        dcc.Graph(
+            id=str(stat) + '_results' + str(capital),
+            figure={
+                'data': [
+                    dict(
+                        x=[i for i in range(1, 101)],
+                        y=df[(df.num_decks == d) & (df.capital == capital) & (df.deck_penetration == p) &
+                             (df.player == i)][('step_' + str(stat))],
+                        mode='lines',
+                        name=str(i) + ' player, ' + str(p) + ' deck penetration, ' + str(d) + ' decks'
+                    ) for d in df.num_decks.unique() for p in df.deck_penetration.unique() for i in df.player.unique()
+                ],
+                'layout': dict(
+                    xaxis={'title': 'Num rounds after game start'},
+                    yaxis={'title': 'Mean remaining capital of the player'},
+                    legend={'x': 0, 'y': 0}
+                )
+            }
+        )
+    ])
+
+
+def get_summary_graphs(df, capital):
+    return html.Div([
+        get_summary_graph(df, capital, 'mean', 'Mean remaining capital at a given step for player '
+                                               'with init capital of ' + str(capital)),
+        get_summary_graph(df, capital, 'std', 'Std of remaining capital at a given step for player '
+                                              'with init capital of ' + str(capital))
+    ], style={'columnCount': 2})
+
+
 def get_app(data):
     dashboard = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
     dashboard.layout = html.Div([
         html.H1('Blackjack Simulation'),
+
+        html.H2('1. Summary statistics of all Simulations'),
+        html.Div([get_summary_graphs(data, c) for c in data.capital.unique()]),
+
+        html.H2('2. Look at specific simulations'),
         html.H3('Simulation Settings'),
         get_dropdown('Player', 'player_dd', data.player.unique()),
         get_dropdown('Num Decks', 'num_decks_dd', data.num_decks.unique()),
         get_dropdown('Deck Penetration', 'penetration_dd', data.deck_penetration.unique()),
         get_dropdown('Player Capital', 'capital_dd', data.capital.unique()),
         generate_table(data)
-    ], style={'width': '80%', 'margin': '2%'})
+    ], style={'width': '80%', 'padding-left': '10%', 'padding-right': '10%'})
 
     return dashboard
 
