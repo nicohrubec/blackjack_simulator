d96b16e06663de9466f5795844c517d8faf7172e
Nico Hrubec <nicolas.hrubec@outlook.com>
2020-04-09 20:55:33 +0200

    Add: Card Counter.

--- src/game_simulation/players.py
+++ src/game_simulation/players.py
@@ -1,5 +1,6 @@
 from src import configs
 import pandas as pd
+import numpy as np
 
 
 def player_factory(player_type, capital):
@@ -7,6 +8,8 @@ def player_factory(player_type, capital):
         return BasicPlayer(init_capital=capital)
     elif player_type == 'strategic':
         return StrategicPlayer(init_capital=capital, file_name='thorp_strategy.xlsx')
+    elif player_type == 'counter':
+        return CountingPlayer(init_capital=capital, file_name='thorp_strategy.xlsx', bet_unit=5)
     else:
         raise ValueError('There is no such player.')
 
@@ -29,6 +32,9 @@ class Player(object):
 
     def get_capital(self):
         return self.capital
+
+    def is_counter(self):
+        return False
 
 
 class BasicPlayer(Player):
@@ -62,7 +68,7 @@ class StrategicPlayer(Player):
         self.strategy_card.columns = [str(col) for col in self.strategy_card.columns]  # convert columns to string
         self.strategy_card.index = self.strategy_card.index.map(str)  # convert index to string
 
-    def bet(self):  # naive betting
+    def bet(self, *args, **kwargs):  # naive betting
         if self.capital > 5:
             self.capital -= 5
             return 5
@@ -97,3 +103,40 @@ class StrategicPlayer(Player):
                 return self.strategy_card.loc[player_selector, str(dealer_cards)]
             else:
                 return self.strategy_card.loc[str(player_value), str(dealer_cards)] if player_value < 21 else 'S'
+
+
+class CountingPlayer(StrategicPlayer):
+    def __init__(self, init_capital, file_name, bet_unit):
+        super().__init__(init_capital, file_name)
+        self.bet_unit = bet_unit
+        self.running_count = 0
+        self.num_seen_cards = 0
+
+    def bet(self, num_decks, *args, **kwargs):
+        super(CountingPlayer, self).bet(num_decks, *args, **kwargs)
+        return (self.get_true_count(num_decks) - 1) * self.bet_unit
+
+    def update_count(self, *args):
+        for cards in args:
+            for card in cards:
+                self.running_count += self.get_card_value(card)
+
+            self.num_seen_cards += len(cards)
+
+    @staticmethod
+    def get_card_value(card):
+        if card == 1 or card == 11 or card == 10:
+            return 1
+        elif card < 7:
+            return -1
+        else:
+            return 0
+
+    def get_true_count(self, num_decks):
+        num_played_decks = np.round(self.num_seen_cards / 52)
+        remaining_decks = num_decks - num_played_decks
+
+        return self.running_count / remaining_decks
+
+    def is_counter(self):
+        return True
